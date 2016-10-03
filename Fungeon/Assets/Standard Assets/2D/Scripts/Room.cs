f61dd094127932fa79dfebbea60016e166cd7e23
Mark Obeldobel <mjo5501@rit.edu>
2016-10-03 17:22:14 -0400

    Mark's Scene with horizontal procedural generation.

---
+++ Fungeon/Assets/Standard Assets/2D/Scripts/Room.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+    public GameObject room;
+    Vector2 position;
+    int roomNumber;
+    float scaleX;
+    float scaleY;
+
+	// Use this for initialization
+	void Start () {
+        scaleX = room.transform.localScale.x;
+        scaleY = room.transform.localScale.y;
+	}
+
+    public void SetRoomNumber(int n)
+    {
+        roomNumber = n;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+}
---
+++ Fungeon/Assets/Standard Assets/2D/Scripts/RoomManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoomManager : MonoBehaviour {
+
+    public List<GameObject> roomSet = new List<GameObject>();
+    static public int numRooms = 2;
+    static public GameObject[] rooms = new GameObject[numRooms];
+    static public float[] roomsWidth = new float[numRooms];
+    static public float[] roomsHeight = new float[numRooms];
+
+    static private Vector2 initialPosition;
+
+
+	// Use this for initialization
+	void Start () {
+        initialPosition = new Vector2(20, 0);
+        SpawnRooms();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void SpawnRooms()
+    {
+        //select all possible rooms
+        rooms = GameObject.FindGameObjectsWithTag("Room");
+
+        //add each room to a collection
+        foreach (GameObject room in rooms)
+        {            
+            roomSet.Add(room);
+        }
+
+        //define the rooms
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            int randRoom = Random.Range(0, roomSet.Count);
+            rooms[i] = roomSet[randRoom];
+            roomsWidth[i] = rooms[i].GetComponent<BoxCollider2D>().size.x;
+            roomsHeight[i] = rooms[i].GetComponent<BoxCollider2D>().size.y;
+        }
+
+        //Instantiate the roomss
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            if (i == 0)
+            {
+                MonoBehaviour.Instantiate(rooms[i], initialPosition, Quaternion.identity);
+            }
+            else
+            {
+                MonoBehaviour.Instantiate(rooms[i], new Vector2(initialPosition.x + roomsWidth[i-1] + (roomsWidth[i] - roomsWidth[i-1])/2, initialPosition.y), Quaternion.identity);
+            }
+        }
+    }
+}

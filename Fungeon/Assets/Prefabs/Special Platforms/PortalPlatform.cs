3dab55bc06601232c3016b99bcbd4049952ee555
MattTurcz <mat3571@rit.edu>
2016-10-12 23:25:47 -0400

    Check out my scene to see the new and updated levels.

---
+++ Fungeon/Assets/Prefabs/Special Platforms/PortalPlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets._2D
+{
+
+    public class PortalPlatform : MonoBehaviour
+    {
+        public GameObject target;
+        //public char jump;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            //if (jump == 'f')
+            //{
+                if (other.tag == "Player")
+                {
+                    //target.GetComponent<PortalPlatform>().jump = 't';
+                    other.gameObject.transform.position = new Vector2(target.transform.position.x, target.transform.position.y - 2);
+                }
+            //}
+        }
+
+       //private void OnTriggerExit2D(Collider2D other)
+       //{
+       //    if (other.tag == "Player")
+       //    {
+       //        jump = 'f';
+       //    }
+       //}
+    }
+}

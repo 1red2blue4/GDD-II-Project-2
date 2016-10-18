2adb87976beb9af9d289640ce5fc0145d3c03245
Mark Obeldobel <mjo5501@rit.edu>
2016-10-18 08:30:08 -0400

    Atmospheric Mode

---
+++ Fungeon/Assets/Scripts/Rooms/ChangeLighting.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLighting : MonoBehaviour {
+
+    public Light[] lights;
+    public FlockManager[] enemies;
+
+	// Use this for initialization
+	void Start () {
+        lights = this.GetComponentsInChildren<Light>();
+        enemies = this.GetComponentsInChildren<FlockManager>();
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!enemies[0])
+        {
+            foreach (Light light in lights)
+            {
+                light.color = Color.gray;
+            }
+        }
+	}
+}

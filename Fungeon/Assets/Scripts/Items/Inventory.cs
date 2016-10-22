e082f28606c97255a0d678b40dddb7b872a75119
Kevin Idzik <kdi4293@rit.edu>
2016-10-22 17:26:51 -0400

    UI and Controller Updates

--- Fungeon/Assets/Scripts/Items/Inventory.cs
+++ Fungeon/Assets/Scripts/Items/Inventory.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Inventory : MonoBehaviour //KI
 {
-    int[] items = new int[5]; //Create an array to hold the items
+    int[] items = new int[10]; //Create an array to hold the items
 
 	void Start() //Use this for initialization
     {

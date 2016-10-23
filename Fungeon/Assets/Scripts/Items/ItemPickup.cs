6f7f35f20e9e993dd4255e1f31227ee99e27814c
Mark Obeldobel <mjo5501@rit.edu>
2016-10-23 15:18:54 -0400

    Some small changes to the scene and added sounds.

--- Fungeon/Assets/Scripts/Items/ItemPickup.cs
+++ Fungeon/Assets/Scripts/Items/ItemPickup.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class ItemPickup : MonoBehaviour //KI
 {
+    public AudioSource[] soundEffects;
+
     void Start() //Use this for initialization
     {
-
+        soundEffects = new AudioSource[5];
+        soundEffects = GetComponents<AudioSource>();
     }
 
     void Update() //Update is called once per frame
@@ -18,6 +21,7 @@ public class ItemPickup : MonoBehaviour //KI
         if(that.gameObject.tag == "item") //If the player is colliding with the item
         {
             Destroy(that.gameObject); //Destroy the item
+            soundEffects[4].Play();
         }
     }
 }

6c9b37f9dd2d12c0834622de852721fb06fa6712
Kevin Idzik <kdi4293@rit.edu>
2016-10-22 21:39:51 -0400

    Item Pickup Behavior Changes and Repo Cleanup

--- Fungeon/Assets/Scripts/Items/ItemPickup.cs
+++ Fungeon/Assets/Scripts/Items/ItemPickup.cs
@@ -1,22 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic; //List
 
 public class ItemPickup : MonoBehaviour //KI
 {
-    void Start() //Use this for initialization
-    {
-
-    }
-
-    void Update() //Update is called once per frame
-    {
-
-    }
+    [SerializeField] private UnityStandardAssets._2D.PlatformerCharacter2D playerControlScript; //The player's control script 
+    private List<string> inventory = new List<string>(); //Create a list to hold the items
 
     void OnTriggerStay2D(Collider2D that) //If the player collides with something
     {
-        if(that.gameObject.tag == "item") //If the player is colliding with the item
+        if(that.gameObject.tag == "item" && playerControlScript.CanAttack == true) //If the player is colliding with the item
         {
+            if (inventory.Count < 10) //If there is no item in the last inventory slot
+            {
+                inventory.Add(that.gameObject.name); //Add the item to the list
+            }
+
             Destroy(that.gameObject); //Destroy the item
         }
     }

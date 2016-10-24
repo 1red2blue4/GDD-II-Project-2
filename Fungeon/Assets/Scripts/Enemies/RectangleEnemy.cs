460be6814a07d723d3b77869f2f1a114ec4856e2
Charles Williams <crw4147@rit.edu>
2016-10-23 20:00:50 -0400

    Enemies Patrol

--- Fungeon/Assets/Scripts/Enemies/RectangleEnemy.cs
+++ Fungeon/Assets/Scripts/Enemies/RectangleEnemy.cs
@@ -5,13 +5,14 @@ public class RectangleEnemy : Enemy {
 
     //-----------------------VARIABLE DECLARATION------------------------
     // variables to track speed
-    public float patrolSpeed = 0.5f;
-    public float pursueSpeed = 3.0f;
-    public float attackSpeed = 6.0f;
+    public float patrolSpeed;
+    public float pursueSpeed;
+    public float attackSpeed;
 
     // bools for tracking states
     private bool playerDetected = false;
     private bool edgeDetected = true;
+    private int direction = 1;
 
     // for raycasting
     public float detectPlayerDistance = 10.0f;
@@ -28,15 +29,25 @@ public class RectangleEnemy : Enemy {
 	// Update is called once per frame
 	override public void Update () {
 
-        if (DetectEdge() == true)
-        {
-            Debug.Log("Edge detected");
-        }
+        Patrol();
 
-        // debug stuff
-	    //Debug.DrawLine(transform.position, new Vector3(transform.position.x + 0.65f, (transform.position.y - enemySprite.bounds.size.y / 2) - 0.15f ,  0), Color.white);
-        Debug.DrawRay(transform.position, transform.right, Color.white);
-        Debug.DrawRay(transform.position, -transform.right, Color.white);
+        //if (DetectRightEdge() == true)
+        //{
+        //    Debug.Log("Right Edge detected");
+        //}
+        //if (DetectLeftEdge() == true)
+        //{
+        //    Debug.Log("Left Edge detected");
+        //}
+        //if (DetectLeftPlayer() == true)
+        //{
+        //    Debug.Log("Player Left");
+        //}
+        //if (DetectRightPlayer() == true)
+        //{
+        //    Debug.Log("Player Right");
+        //}
+
 	}
 
     /// <summary>
@@ -47,7 +58,53 @@ public class RectangleEnemy : Enemy {
     /// </summary>
     public void Patrol()
     {
+        bool left = DetectLeftEdge();
+        bool right = DetectRightEdge();
 
+        Debug.Log("Right: " + right);
+        Debug.Log("Left: " + left);
+
+        // make series of if statements to check where to partol
+        // patrol right
+        if (direction == 1 && right == true)
+        {
+            Debug.Log("Going Right...");
+            rb.velocity = new Vector3(patrolSpeed, 0);
+        }
+
+        // turn left
+        if (direction == 1 && right == false)
+        {
+            Flip();
+            Debug.Log("Turning Left...");
+            direction = -1;
+        }
+
+        // patrol left
+        if (direction == -1 && left == true)
+        {
+            Debug.Log("Going Left...");
+            rb.velocity = new Vector3(-patrolSpeed, 0);
+        }
+
+        // turn right
+        if (direction == -1 && left == false)
+        {
+            Flip();
+            Debug.Log("Turning Right...");
+            direction = 1;
+        }
+    }
+
+    /// <summary>
+    /// flips the enemy
+    /// </summary>
+    public void Flip()
+    {
+        // flip enemy
+        Vector3 theScale = this.GetComponentInChildren<SpriteRenderer>().transform.localScale;
+        theScale.x *= -1;
+        this.GetComponentInChildren<SpriteRenderer>().transform.localScale = theScale;
     }
 
     /// <summary>
@@ -55,7 +112,7 @@ public class RectangleEnemy : Enemy {
     /// their position this method will check if the enemy is
     /// about to fall off of a platform
     /// </summary>
-    public bool DetectEdge()
+    public bool DetectRightEdge()
     {
         // calculate the vectors used in raycasting
         Vector3 edgeDetect = new Vector3(transform.position.x + 1.0f, -1 * (transform.position.y - enemySprite.bounds.size.y / 2) - 0.15f, 0);
@@ -65,9 +122,54 @@ public class RectangleEnemy : Enemy {
 
         Debug.DrawRay(new Vector3(transform.position.x + 0.4f, transform.position.y, 0), edgeDetectNorm * 1.0f, Color.white);
 
-        Debug.Log(edgeCheck.collider.transform.parent.tag);
+        //Debug.Log(edgeCheck.collider.transform.parent.tag);
 
-        if (edgeCheck.collider.transform.parent.tag == "platform")
+        //if (edgeCheck.collider.transform.parent.tag == "platform")
+        //{
+        //    return true;
+        //}
+        //else
+        //{
+        //    return false;
+        //}
+
+        if (edgeCheck.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// using a raycast angled down and slightly forward from
+    /// their position this method will check if the enemy is
+    /// about to fall off of a platform
+    /// </summary>
+    public bool DetectLeftEdge()
+    {
+        // calculate the vectors used in raycasting
+        Vector3 edgeDetect = new Vector3(transform.position.x - 1.0f, (transform.position.y - enemySprite.bounds.size.y / 2) - 0.15f, 0);
+        Vector3 edgeDetectNorm = edgeDetect.normalized * -1;
+
+        RaycastHit2D edgeCheck = Physics2D.Raycast(new Vector3(transform.position.x - 0.4f, transform.position.y, 0), edgeDetectNorm, 1.0f);
+
+        Debug.DrawRay(new Vector3(transform.position.x - 0.4f, transform.position.y, 0), edgeDetectNorm * 1.0f, Color.white);
+
+        //Debug.Log(edgeCheck.collider.transform.parent.tag);
+
+        //if (edgeCheck.collider.transform.parent.tag == "platform")
+        //{
+        //    return true;
+        //}
+        //else
+        //{
+        //    return false;
+        //}
+
+        if (edgeCheck.collider != null)
         {
             return true;
         }
@@ -82,8 +184,50 @@ public class RectangleEnemy : Enemy {
     /// the enemy this enemy will check if it can "see" a player
     /// if so, it will enter pursue mode
     /// </summary>
-    public bool DetectPlayer()
+    public bool DetectRightPlayer()
     {
-        return false;
+        // calculate vectors used in raycasting
+        Vector3 playerDetectRight = transform.right;
+        Vector3 playerDetectRightNorm = playerDetectRight.normalized;
+
+        RaycastHit2D playeeRightCheck = Physics2D.Raycast(new Vector3((transform.position.x + (enemySprite.bounds.size.x / 2) + 0.1f), transform.position.y, 0), playerDetectRightNorm, Mathf.Infinity);
+
+        Debug.DrawRay(new Vector3((transform.position.x + (enemySprite.bounds.size.x / 2) + 0.1f), transform.position.y, 0), playerDetectRightNorm * Mathf.Infinity, Color.white);
+
+
+        if(playeeRightCheck.transform.tag == "Player")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    /// <summary>
+    /// using 2 raycasts that shoot off from the left and right of
+    /// the enemy this enemy will check if it can "see" a player
+    /// if so, it will enter pursue mode
+    /// </summary>
+    public bool DetectLeftPlayer()
+    {
+        // calculate vectors used in raycasting
+        Vector3 playerDetectLeft = -transform.right;
+        Vector3 playerDetectLeftNorm = playerDetectLeft.normalized;
+
+        RaycastHit2D playerLeftCheck = Physics2D.Raycast(new Vector3((transform.position.x - (enemySprite.bounds.size.x / 2) - 0.1f), transform.position.y, 0), playerDetectLeftNorm, Mathf.Infinity);
+
+        Debug.DrawRay(new Vector3((transform.position.x - (enemySprite.bounds.size.x / 2) - 0.1f), transform.position.y, 0), playerDetectLeftNorm * Mathf.Infinity, Color.white);
+
+        if (playerLeftCheck.transform.tag == "Player")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }

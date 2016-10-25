00294f58cf0959ab7646fd9d518c24ae10e0f023
Charles Williams <crw4147@rit.edu>
2016-10-24 20:47:59 -0400

    did a lot of stuff
    - enemy scripr updated
    - lag management(hopefully)

--- Fungeon/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Fungeon/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -5,6 +5,7 @@ namespace UnityStandardAssets._2D
 {
     public class Camera2DFollow : MonoBehaviour
     {
+        //
         public Transform target;
         public float damping = 1;
         public float lookAheadFactor = 3;

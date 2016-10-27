56286ec7fdcbddd6aa450cd55c2dc4eb023afe98
pxl1778 <pxl1778@rit.edu>
2016-10-26 23:57:40 -0400

    Title Screen Effects

---
+++ Fungeon/Assets/Prefabs/TitleScreenEffect/TitleScreenEffect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets._2D
+{
+    public class TitleScreenEffect : MonoBehaviour
+    {
+        private float timer;
+        private SpriteRenderer spriteRenderer;
+        private Color prevColor;
+
+        // Use this for initialization
+        void Start()
+        {
+            spriteRenderer = this.GetComponent<SpriteRenderer>();
+            prevColor = spriteRenderer.color;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer >= 10f)
+            {
+                timer = 0.0f;
+            }
+            else
+            {
+                float alpha = timer / 10f;
+                spriteRenderer.color = (new HSBColor(alpha, 1f, .4f)).ToColor();
+            }
+        }
+    }
+}
